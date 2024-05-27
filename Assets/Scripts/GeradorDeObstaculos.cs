963e237524532af8ed3490efd674dbc21a18b865
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-27 09:41:36 -0300

    bird

--- Assets/Scripts/GeradorDeObstaculos.cs
+++ Assets/Scripts/GeradorDeObstaculos.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class GeradorDeObstaculos : MonoBehaviour
 {
-    [SerializeField]
-    private float tempoParaGerar = 3;
-
-    private float cronometro;
-
+    //Onde gerar? Posição do gerador
+    //Quando gerar? tempo
     [SerializeField]
     private GameObject modeloObstaculo;
+
+    [SerializeField]
+    private float tempoParaGerar = 3;
+    private float cronometro;
+
 
     private void Awake()
     {
@@ -26,5 +28,6 @@ public class GeradorDeObstaculos : MonoBehaviour
             GameObject.Instantiate(this.modeloObstaculo, this.transform.position, Quaternion.identity);
             this.cronometro = this.tempoParaGerar;
         }
+        
     }
 }
--- Assets/Scripts/obstaculo.cs
+++ Assets/Scripts/obstaculo.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class obstaculo : MonoBehaviour
+public class Obstaculo : MonoBehaviour
 {
     [SerializeField]
-    private float velocidade = 1.5f;
+    private float velocidade = 0.6f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.Translate(Vector3.left * Time.deltaTime);   
+        this.transform.Translate(Vector3.left * velocidade * Time.deltaTime);
+        
     }
 }
