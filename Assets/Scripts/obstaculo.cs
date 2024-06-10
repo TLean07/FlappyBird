67a6b398a8f6bfb5dd1d97cc6d293ebc5aa73d06
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-10 10:54:22 -0300

    flappybirf

--- Assets/Scripts/obstaculo.cs
+++ Assets/Scripts/obstaculo.cs
@@ -9,20 +9,26 @@ public class obstaculo : MonoBehaviour
 
     [SerializeField]
     private float variacaoposicaoY;
-    void Start()
-    {
-        
-    }
+    private Vector3 posicaoPassaro;
+    private bool pontuei; 
 
     private void Awake()
     {
         this.transform.Translate(Vector3.up * Random.Range(-variacaoposicaoY, variacaoposicaoY));
     }
 
+    private void Start(){
+        this.posicaoPassaro = GameObject.FindObjectOfType<Bird>().transform.position;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        this.transform.Translate(Vector3.left * Time.deltaTime);   
+        this.transform.Translate(Vector3.left * velocidade * Time.deltaTime);
+        if(this.pontuei && this.transform.position.x < this.posicaoPassaro.x){
+            Debug.Log("Pontuou!");
+            this.pontuei = true;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
